b69e4526a676f74e9e90eff037f25e92fc55221d
Alexander Oviedo <alexanderovie@gmail.com>
2025-12-12 15:19:53 -0500

    feat: centralizar tipo MDXComponents y mejorar estructura del blog

--- src/lib/blog/index.ts
+++ src/lib/blog/index.ts
@@ -4,3 +4,4 @@
 export { getAllPosts } from "./getAllPosts";
 export { getPostBySlug } from "./getPostBySlug";
 export type { BlogPost, BlogPostMetadata, BlogFrontmatter } from "./types";
+export type { MDXComponents } from "./mdx-types";
