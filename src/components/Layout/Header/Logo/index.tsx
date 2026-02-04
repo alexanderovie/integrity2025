c5d8f10c9c2dae6b095103b1a5940e6208074faa
Alexander Oviedo <alexanderovie@gmail.com>
2026-02-04 16:48:36 -0500

    fix: invert navbar logo in dark mode

--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -5,22 +5,12 @@ const Logo = () => {
     return (
         <Link href="/">
             <Image
-                src="/images/logo/logo.svg"
+                src="/images/logo/integrity-navbar.png"
                 alt="Integrity Clean Solutions – Residential & Commercial Cleaning in Orlando, FL"
                 width={1600}
                 height={538}
-                className="h-[3.75rem] w-auto dark:hidden"
+                className="h-[3.75rem] w-auto dark:invert"
                 priority
-                quality={90}
-            />
-            <Image
-                src="/images/logo/dark-logo.svg"
-                alt="Integrity Clean Solutions – Residential & Commercial Cleaning in Orlando, FL"
-                width={1600}
-                height={538}
-                className="hidden h-[3.75rem] w-auto dark:block"
-                priority
-                quality={90}
             />
         </Link>
     );
