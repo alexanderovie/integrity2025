53b642548752d9ceb70466a2c8334bc028c4f959
Alexander Oviedo <alexanderovie@gmail.com>
2026-03-13 13:13:05 -0400

    config: actualizar proyecto de Sanity a nuevo account\n\n- Nuevo Project ID: l4t851dy\n- Nuevos tokens de API actualizados en Vercel\n- Variables de entorno locales actualizadas

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { structureTool } from "sanity/structure";
 import { defineConfig } from "sanity";
 import { postSchema } from "@/sanity/schema/post";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "missing-project-id";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "l4t851dy";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineConfig({
